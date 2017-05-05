e0309099a7627657bd48dd8544a10b25aa3fa847
15017185 <15017185@myrp.edu.sg>
2017-05-05 14:45:10 +0800

    main

--- app/src/main/java/com/example/a126308/p03_classjournal/Info.java
+++ app/src/main/java/com/example/a126308/p03_classjournal/Info.java
@@ -1,13 +1,32 @@
 package com.example.a126308.p03_classjournal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Info extends AppCompatActivity {
+
+    Button btnRP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        btnRP = (Button) findViewById(R.id.buttonInfo);
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
     }
 }
