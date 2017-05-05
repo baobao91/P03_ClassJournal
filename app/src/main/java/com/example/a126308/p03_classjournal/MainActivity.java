a8ba2e8993231fb1a4825ceff094115ef2aff309
126308 <126308@myrp.edu.sg>
2017-05-05 12:52:28 +0800

    Done

--- app/src/main/java/com/example/a126308/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/example/a126308/p03_classjournal/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.a126308.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv347;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv347 = (TextView)findViewById(R.id.textView347);
+
+        tv347.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, Info.class);
+                startActivity(i);
+            }
+        });
     }
 }
